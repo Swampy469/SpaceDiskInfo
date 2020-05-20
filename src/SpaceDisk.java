3dec9796ca05b99f022182448a73ecfaf67ddbe8
Swampy469 <alessiomarano339@gmail.com>
2020-05-21 01:12:57 +0200

    Renamed some variable

--- src/SpaceDisk.java
+++ src/SpaceDisk.java
@@ -46,23 +46,23 @@ public class SpaceDisk {
         panel.add(total_label);
         total_label.setBounds(0,25,600,50);
         total_label.setForeground(Color.YELLOW);
-        total_label.setFont(new Font("Segui UI", Font.BOLD, size));
+        total_label.setFont(new Font("font", Font.BOLD, size));
 
         panel.add(available_label);
         available_label.setBounds(0, total_label.getSize().height+15, 600, 30);
         available_label.setForeground(Color.green);
-        available_label.setFont(new Font("Miao", Font.BOLD, size));
+        available_label.setFont(new Font("font", Font.BOLD, size));
 
         panel.add(used_label);
         used_label.setBounds(0, total_label.getSize().height+45, 600, 30);
         used_label.setForeground(Color.red);
-        used_label.setFont(new Font("Miao", Font.BOLD, size));
+        used_label.setFont(new Font("font", Font.BOLD, size));
 
         panel.add(bottone);
-        bottone.setBounds((frame.getSize().width /2) -80, 150 , 100 , 20);
+        bottone.setBounds((frame.getSize().width /2) -80, 120 , 100 , 20);
 
         panel.add(text);
-        text.setBounds((frame.getSize().width /2) -80 , total_label.getSize().height+75, 100,25);
+        text.setBounds((frame.getSize().width /2) -80 , total_label.getSize().height+95, 100,25);
         text.setEditable(true);
         text.setForeground(Color.WHITE);
         text.setBackground(new Color(47,47,47));
