87c552c02bb0d5eb5f65672b27d6f8b74fc98531
Swampy469 <alessiomarano339@gmail.com>
2020-05-21 13:32:11 +0200

    Cleaning up the code

--- src/SpaceDisk.java
+++ src/SpaceDisk.java
@@ -24,16 +24,19 @@ public class SpaceDisk {
 
        File partition_name = new File(save); // Insert the letter of your partition (ex: 'C:' , 'D:')
 
-        total_label.setText(String.format("Total Space: %.2f GB", (double)partition_name.getTotalSpace()/1073741824)); //Total
-        available_label.setText(String.format("Available Space: %.2f GB", (double)partition_name.getFreeSpace()/1073741824)); // Available
-        used_label.setText(String.format("Used Space: %.2f GB", (double)partition_name.getUsableSpace()/1073741824)); //Used
+        label_1.setText(String.format("Total Space: %.2f GB", (double)partition_name.getTotalSpace()/1073741824)); //Total
+        label_2.setText(String.format("Available Space: %.2f GB", (double)partition_name.getFreeSpace()/1073741824)); // Available
+        label_3.setText(String.format("Used Space: %.2f GB", (double)partition_name.getUsableSpace()/1073741824)); //Used
 
     }
 
     public static void main(String[] args){
-        int size=25;
 
-        frame.setBounds(0,0,440,280);
+       int height = 280;
+       int width = 440;
+        int text_size=25;
+
+        frame.setBounds(0,0,width,height);
         frame.setLayout(null);
         frame.setVisible(true);
 
@@ -44,25 +47,25 @@ public class SpaceDisk {
         panel.setVisible(true);
 
         panel.add(total_label);
-        total_label.setBounds(0,25,600,50);
+        total_label.setBounds(0,frame.getY(),frame.getWidth(),30);
         total_label.setForeground(Color.YELLOW);
-        total_label.setFont(new Font("font", Font.BOLD, size));
+        total_label.setFont(new Font("font", Font.BOLD, text_size));
 
         panel.add(available_label);
-        available_label.setBounds(0, total_label.getSize().height+15, 600, 30);
+        available_label.setBounds(0, frame.getY()+35, frame.getWidth(), 30);
         available_label.setForeground(Color.green);
-        available_label.setFont(new Font("font", Font.BOLD, size));
+        available_label.setFont(new Font("font", Font.BOLD, text_size));
 
         panel.add(used_label);
-        used_label.setBounds(0, total_label.getSize().height+45, 600, 30);
+        used_label.setBounds(0, frame.getY()+65, frame.getWidth(), 30);
         used_label.setForeground(Color.red);
-        used_label.setFont(new Font("font", Font.BOLD, size));
+        used_label.setFont(new Font("font", Font.BOLD, text_size));
 
         panel.add(bottone);
         bottone.setBounds((frame.getSize().width /2) -80, 120 , 100 , 20);
 
         panel.add(text);
-        text.setBounds((frame.getSize().width /2) -80 , total_label.getSize().height+95, 100,25);
+        text.setBounds((frame.getSize().width /2) -90 , frame.getY()+95, 120,25);
         text.setEditable(true);
         text.setForeground(Color.WHITE);
         text.setBackground(new Color(47,47,47));
@@ -72,9 +75,8 @@ public class SpaceDisk {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(text.getText().isEmpty()){
-                    text.setText("Write a partition name ");
+                    text.setText("Write a partition letter ");
                 }else{
-                    String save;
                     calcolo(total_label,available_label,used_label);
                 }
             }
